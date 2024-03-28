4d4e3b2fc1c540d49f40bed0a200529ae47dcdff
James <lawrencejames083@gmail.com>
2024-03-28 11:23:55 +0200

    Completed the FAQ section : Both HTML and  CSS as well as Javascript logic to hide and view responses.

---
+++ js/main.js
@@ -0,0 +1,40 @@
+// FAQ accordion
+// Wait for page to load
+document.addEventListener("DOMContentLoaded", () => {
+    // variable to access container link
+    const faqContainer = document.querySelector(".faq-content");
+    // Add event listener on parent
+    faqContainer.addEventListener("click", (e) => {
+        // Create logic to only select header using event delegation
+        const groupHeader = e.target.closest('.faq-group-header');
+
+        if(!groupHeader) return;
+        
+        // create variables for each node within faq-group
+        const group = groupHeader.parentElement;
+        const groupBody = group.querySelector('.faq-group-body');
+        const icon = groupHeader.querySelector('i');
+
+        // Add logic to switch between plus or minus icon
+        icon.classList.toggle('fa-plus');
+        icon.classList.toggle('fa-minus');
+
+        // toggle visibillity of body
+        groupBody.classList.toggle('open');
+
+        // close inactive body elements
+        const otherGroups = faqContainer.querySelectorAll('.faq-group');
+        
+        otherGroups.forEach((otherGroup) => {
+            if(otherGroup !== group){
+                const otherGroupBody = otherGroup.querySelector('.faq-group-body');
+                const otherIcon= otherGroup.querySelector('.faq-group-header i');
+
+                otherGroupBody.classList.remove('open');
+                otherIcon.classList.remove('fa-minus');
+                otherIcon.classList.add('fa-plus');
+            }
+        } );
+
+    })
+});
